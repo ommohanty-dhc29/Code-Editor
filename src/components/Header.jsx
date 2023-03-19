c1eb7394c395dc4951fc9ecaaf08170f8df52745
Om Mohanty <ommohanty@gmail.com>
2023-03-19 19:13:10 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+
+import { AppBar, Toolbar, styled } from "@mui/material"
+
+const Container = styled(AppBar)`
+    background: #060606;
+    position: static;
+    border-bottom: 1px solid #2f2f2f;
+    height: 9vh;
+`;
+
+const Header = () => {
+    return (
+        <Container />
+    )
+}
+
+export default Header
